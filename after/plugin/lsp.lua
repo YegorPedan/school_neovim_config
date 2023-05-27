c354e678e856b526531f60f848bc047b1f0b3a24
Yegor <wzxv_@outlook.com>
2023-05-27 20:34:07 +0300

    rebind <leader>D -> <leader>d

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -74,9 +74,7 @@ end
 require("mason-null-ls").setup({
 	-- list of formatters & linters for mason to install
 	ensure_installed = {
-		"prettier", -- ts/js formatter
 		"stylua", -- lua formatter
-		"eslint_d", -- ts/js linter
 		"clang-format",
 	},
 	-- auto-install configured formatters & linters (with null-ls)
